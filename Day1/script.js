a26aaeb1962322a5b1d4f4a0f48573174003536a
bisscay <bisskay@gmail.com>
2018-12-02 20:41:34 +0100

    added Day1 Part2 soln

--- Day1/script.js
+++ Day1/script.js
@@ -1,5 +1,5 @@
-//	resulting frequency
-const fs = require('fs');
+// //	PART ONE	(adjusting frequency drift)
+ const fs = require('fs');
 
 const freqAdjust = () => {
 	//	accept freqInput	-->	convert it to a String -->	split the string at an new line and \r 
@@ -9,8 +9,39 @@ const freqAdjust = () => {
 
 		const resultingFreq = frequencyArray.reduce((acc, currentV) => acc + currentV);
 
-		console.log(resultingFreq);
+		console.log('PART 1: ', resultingFreq);
 	})
 };
 
+
+
+//	PART TWO	(frequency calibration)
+const freqCalibrate = () => {
+	//	finding the first frequency that occurs twice
+	fs.readFile('./freqInput.txt', (err, data) => {
+	const changes = data.toString().split('\r\n');
+	const frequencyArray = changes.map(splitV => Number(splitV));
+	 
+	let reloop = true;
+	let currentLevel = 0;
+	let freqLevels = [];
+	let repeatLevels = [];	
+	while (reloop) {
+		frequencyArray.forEach(freq => {
+			currentLevel += freq;
+			if (freqLevels.includes(currentLevel)) {
+				repeatLevels.push(currentLevel);
+				reloop = false;
+			}
+			freqLevels.push(currentLevel);
+		});
+	}
+
+		console.log('PART 2: ', repeatLevels[0]);
+	})
+
+}
+
 freqAdjust();
+
+freqCalibrate();
