a88d9a05e7147d20ca39bc3a898e1aaac63faadd
bisscay <bisskay@gmail.com>
2018-12-03 21:49:20 +0100

    included part 2 of day 2

--- Day2/script.js
+++ Day2/script.js
@@ -1,4 +1,4 @@
-//	PART ONE
+//	------------------- PART ONE ---------------------------
 const fs = require('fs');
 
 const boxChecksum = () => {
@@ -51,6 +51,43 @@ const boxChecksum = () => {
 
 		//	console.log(ID + ' count2: ' + countPerLetter2 + '. count3: ' + countPerLetter3 + ' count: ' + countPerLetter);	
 		}); console.log(`boxChecksum is: ${pixie * dust}`);
+
+
+		//	------------------- PART TWO ----------------------------------------------
+		const commonBoxLetters = (arr) => {
+		//	variable to store common letters
+		let commonLetters = '';
+		//	loop to access the words
+		for (let wordIndexA = 0; wordIndexA < arr.length; wordIndexA++) {
+			//	loop to access the remaining words for comparism one at a time
+			//	NB: the word index will start on the next position from word A
+			for (let wordIndexNext = wordIndexA + 1; wordIndexNext < arr.length; wordIndexNext++) {
+				// loop to access the letters of each word
+				// NB: word A and each next word will compare on thesame letter index
+				//	the loop will be limited by the length of the current word
+				for (let letterIndex = 0; letterIndex < arr[wordIndexA].length; letterIndex++) {
+					//	perform the comparism on each letter on each words(A and next)
+					//	if a match is found, add the letter to the commonletters string, else dont do any thing
+					if (arr[wordIndexA][letterIndex] === arr[wordIndexNext][letterIndex]) {
+						commonLetters += arr[wordIndexA][letterIndex];
+					}
+				}
+				//	check the length of the word added, 
+				//	if it is equal to the original word minus one, we return this word 
+				//	else we set our commonLetters to an empty string and continue the loop for the next word
+				if (commonLetters.length === arr[wordIndexA].length - 1) {
+					return (`Common Letters is: ${commonLetters}`); //return commonLetters;	//	NB: if you dont return, the
+				} else {
+					commonLetters = '';
+				}
+			}
+		} 
+		commonBoxLetters(boxIDs);
+
+		//commonBoxLetters(test);
+
+	};
+console.log(commonBoxLetters(boxIDs));
 	});
 };
 
